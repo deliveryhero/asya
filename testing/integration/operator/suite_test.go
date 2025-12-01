867608db6abd4ba9bc042ad6c9a744a864a88173
Artem Yushkovskiy <artem.yushkovskiy@deliveryhero.com>
2025-12-01 21:35:24 +0000

    Fix operator

--- testing/integration/operator/suite_test.go
+++ testing/integration/operator/suite_test.go
@@ -170,7 +170,7 @@ func TestMain(m *testing.M) {
 		Client:            k8sManager.GetClient(),
 		Scheme:            k8sManager.GetScheme(),
 		TransportRegistry: transportRegistry,
-		TransportFactory:  transports.NewFactory(k8sManager.GetClient(), transportRegistry),
+		TransportFactory:  transports.NewFactory(k8sManager.GetClient(), transportRegistry, "default"),
 	}).SetupWithManager(k8sManager)
 	if err != nil {
 		panic(err)
